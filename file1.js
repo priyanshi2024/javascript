295c0247764aa900fa3d9e96c76817709928d3ea
priyanshi porwal <74495039+priyanshi2024@users.noreply.github.com>
2023-02-24 14:29:20 +0530

    javascript poc

---
+++ file1.js
@@ -0,0 +1,155 @@
+//"use strict"
+// console.log("Hello world");
+
+// var firstName="Priyanshi";
+// console.log(firstName);
+
+// var firstName="Porwal";
+// console.log(firstName);
+
+// let firstName="Priyanshi";
+// console.log(firstName);
+
+// let firstName="Porwal";
+// console.log(firstName);
+
+// const pi=3.14;
+// console.log(pi*2);
+// pi=20;
+
+// let firstName="34";
+// console.log(firstName[7]);
+// console.log(firstName.length);
+// console.log(firstName.toUpperCase);
+// console.log(firstName.slice(1));
+// console.log(typeof firstName);
+// let num=3.62;
+// console.log(typeof num);
+// num=String(num)
+// num=num+"";
+// console.log(typeof num);
+// firstName=+firstName;
+// console.log(typeof firstName);
+
+// firstName=(Number)firstName;
+//console.log(typeof firstName);
+
+// let firstName="Priyanshi";
+// let lastName="Porwal";
+// console.log(firstName+lastName);
+// console.log(firstName.concat(lastName));
+
+// let num=34;
+// let num1="34"
+// console.log(num==num1);
+// console.log(num===num1);
+
+// let age;
+// if(age<20){
+//     console.log("Age ${age} less than 20");
+// }
+// else{
+//     console.log("More than 20");
+// }
+
+// for(let i=0;i<=9;i++){
+//     console.log(i);
+// }
+
+
+// let i=0;
+// while(i<=9){
+//     console.log(i);
+//     i++;
+// }
+
+// let i=0
+// do{
+//     i++;
+//     console.log(i);
+// }while(i<=9);
+
+
+// let i;
+// prompt("Guess a number :");
+// if(i){
+//     console.log(i);
+//     console.log("Correct");
+// }
+// else{
+//     console.log("Very close");
+// }
+
+// let mixed=["hello",23,3.7,null,undefined];
+// console.log(mixed);
+// mixed.push("World");
+// console.log(mixed);
+// mixed.pop();
+// console.log(mixed);
+// mixed.shift();
+// console.log(mixed);
+// mixed.unshift("Hello");
+
+
+const person={name:"Priyanshi",age:22}
+console.log(person);
+console.log(person.name);
+console.log(person["age"]);
+
+
+const Person={
+
+    name:"Priyanshi",
+    age: 22,
+    course: "Full time"
+
+}
+console.log(Person);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
